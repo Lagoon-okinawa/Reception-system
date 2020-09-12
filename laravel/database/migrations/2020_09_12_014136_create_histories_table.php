4d4256e06d5c192fe4e22551d7746cf6bab1ba6f
Masaki <yan.yan.yan@icloud.com>
2020-09-12 11:36:55 +0900

    historiesテーブルのマイグレーション作成

---
+++ laravel/database/migrations/2020_09_12_014136_create_histories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHistoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('histories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('purpose');
+            $table->timestamp('check_in')->nullable();
+            $table->timestamp('check_out')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('histories');
+    }
+}
