98d1beafa0688140e2c2edd3b79c95883fca9ee2
mejamoleo <reonarudo11919273@gmail.com>
2020-09-20 13:10:27 +0900

    :+1: シーダーの作成

--- laravel/database/factories/HistoryFactory.php
+++ laravel/database/factories/HistoryFactory.php
@@ -7,20 +7,23 @@ use Faker\Generator as Faker;
 use Carbon\Carbon;
 
 $factory->define(History::class, function (Faker $faker) {
-    $now = Carbon::now();
-    $is_check_out = $faker->numberBetween(0, 1);
+    // 13<=A<=B<=21
+    // 日付を決める
+    $open_at = Carbon::today()
+        ->subDays(rand(1, 365))
+        ->addHours(13);
 
-    if ($is_check_out) {
-        $rand_minutes = $faker->numberBetween(0, 60 * 8);
-        $check_out_time = $now->addMinutes($rand_minutes);
-    } else {
-        $check_out_time = null;
-    }
+    $check_in_minutes = rand(0, 480 - 1); // -1は21:00にチェックインしない対策
+    $check_in_at = $open_at->addMinutes($check_in_minutes);
+    $check_out_at = clone $check_in_at;
+
+    $staying_minutes = rand(1, 480 - $check_in_minutes); // 滞在分数を定義する
+    $check_out_at = $check_out_at->addMinutes($staying_minutes);
 
     return [
         'user_id' => $faker->numberBetween(1, 100),
         'purpose' => $faker->numberBetween(0, 5),
-        'check_in' => $now,
-        'check_out' => $check_out_time,
+        'check_in' => $check_in_at,
+        'check_out' => $check_out_at,
     ];
 });
--- laravel/database/seeds/HistoriesTableSeeder.php
+++ laravel/database/seeds/HistoriesTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\History;
+use Carbon\Carbon;
 
 
 class HistoriesTableSeeder extends Seeder
@@ -13,6 +14,26 @@ class HistoriesTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(History::class, 200)->create();
+        // 過去の日付のデータを作成
+        // factory(History::class, 1000)->create();
+        factory(History::class, 3)->create();
+
+        // 今日の日付のデータを作成
+        // チェックアウトがあるデータの作成
+        $check_in_at = Carbon::today()->addHours(13)->addMinutes(rand(0, 60 * 3)); // 営業開始から3時間以内にくるとしている
+        $check_out_at = $check_in_at->addMinutes(rand(0, 60 * 3)); // 滞在時間は3時間いない
+        factory(History::class, 2)->create([
+            'user_id' => rand(1, 100),
+            'purpose' => rand(0, 5),
+            'check_in' => $check_in_at,
+            'check_out' => $check_out_at,
+        ]);
+        // チェックアウトがないデータの作成
+        factory(History::class, 2)->create([
+            'user_id' => rand(1, 100),
+            'purpose' => rand(0, 5),
+            'check_in' => $check_in_at,
+            'check_out' => null,
+        ]);
     }
 }
